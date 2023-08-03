35bc5adb6e7a1fe3ec5962b9a97610a71423f268
Bruno Martins <bgcngm@gmail.com>
2023-08-03 11:31:51 +0100

    sm8450-common: Let build system copy audio manifests

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -1064,8 +1064,6 @@ PRODUCT_PACKAGES += \
     dataconnection-saidl \
     manifest_android.hardware.drm@1.4-service.widevine \
     manifest_lahaina_identity \
-    manifest_non_qmaa \
-    manifest_non_qmaa_extn \
     manifest_vendor.xiaomi.hardware.mlipay \
     qtiradio-saidl \
     vendor.qti.diag.hal.service \
