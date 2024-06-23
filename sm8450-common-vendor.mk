5e1e54ae74261ed1f61eb4ba83b87c70ad807759
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    sm8450-common: Declare IMS and WFD libs as symlinks during extraction

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -1584,3 +1584,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2-service \
     vendor.qti.hardware.vpp@2.0-service \
     vendor.xiaomi.sensor.communicate@1.0_manifest
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libwfdnative_libwfdnative_symlink64
