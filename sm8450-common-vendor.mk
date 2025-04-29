d56f1f20cb86454d600c6da172f8b4fa75e3d72b
someone5678 <someone5678@users.noreply.github.com>
2025-04-29 15:01:36 +0530

    sm8450-common: Import QCOM audio effects from OnePlus 9R
    From:
    qssi-user 14 UKQ1.230924.001 1702916091049 release-keys
    
    Change-Id: I88bfb997f635ae86eaa0cde451e61816ebc2d0d7

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -587,6 +587,9 @@ PRODUCT_PACKAGES += \
     sensors.touch.detect \
     sensors.ultrasoundproximity \
     libasphere \
+    libqcbassboost \
+    libqcreverb \
+    libqcvirt \
     vendor.display.color@1.0 \
     vendor.display.color@1.1 \
     vendor.display.color@1.2 \
