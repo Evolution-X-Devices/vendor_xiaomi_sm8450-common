38e6219a52132bf260d876f0d095ba3b13399834
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    sm8450-common: Update WFD system blobs from marvel_g A171WEH.20
    Change-Id: I8000a6ec6c4021042155933e6ce4e3149197dede
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -820,7 +820,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.qccsyshal@1.2 \
     vendor.qti.hardware.qccvndhal@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
-    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
+    vendor.qti.hardware.wifidisplaysession_aidl-V2-ndk \
     vendor.qti.imsrtpservice@3.0 \
     vendor.qti.imsrtpservice@3.1 \
     vendor.qti.qccvndhal_aidl-V1-ndk \
