a65718c8f25a2b676ce79ab5428fede3bdf11c7b
Arian <arian.kulmer@web.de>
2026-06-23 12:46:48 +0200

    sm8450-common: Disable all CLO-specific perf features
    This tried to enable plenty of features which we do not support on
    AOSP, like silky scroll and learning module. It also set some
    properties for CLO additions to lmk, BoostFramework and other changes.
    None of these are relevant for us on AOSP.
    
    The perf stack skips the regular interaction boost when silky scroll is
    enabled, so disable all the CLO features to get the regular interaction
    and launch boosts working when sent via the qcom power aidl.
    
    Change-Id: I0bac510ae71ee0062b81ad499fafc53bef6de6ce

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -172,7 +172,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/commonsysnodesconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/commonsysnodesconfigs.xml \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/perfboostselection.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostselection.xml \
-    vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/targetconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetconfig.xml \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/targetresourceconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetresourceconfigs.xml \
     vendor/xiaomi/sm8450-common/proprietary/vendor/etc/perf/targetsysnodesconfigs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/targetsysnodesconfigs.xml \
